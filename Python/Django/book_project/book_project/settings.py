2b2b5c8472ead545264d00a067a04ff9b653dfcf
JiHyun <90372242+sdoohee@users.noreply.github.com>
2023-10-12 00:59:11 +0900

    Update settings.py

--- Python/Django/book_project/book_project/settings.py
+++ Python/Django/book_project/book_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-yd&u+3ti2swy!0el$wpzau_g+m7c^-n#**y$-mku6w=#&u#hv$'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
